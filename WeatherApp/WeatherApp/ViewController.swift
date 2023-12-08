936e6002d543e5d82bbb5f94a1e159b1c3ec869c
Vasili13 <90265333+Vasili13@users.noreply.github.com>
2023-12-08 12:39:58 +0300

    Initial Commit

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Василий Вырвич on 8.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
